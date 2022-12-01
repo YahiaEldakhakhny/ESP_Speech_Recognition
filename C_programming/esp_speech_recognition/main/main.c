bc43d7093323592f9d030a1910b303fe2780286b
YahiaEldakhakhny <yahia4360673@gmail.com>
2022-12-01 09:23:39 +0200

    listen function is done

--- C_programming/esp_speech_recognition/main/main.c
+++ C_programming/esp_speech_recognition/main/main.c
@@ -17,13 +17,19 @@
 
 #define MODE_RECOGNISE_PIN 18
 #define MODE_TRAIN_PIN 19
+#define LISTEN_PIN 13
 #define ANALOG_PIN ADC1_CHANNEL_6 // analog pin is pin 34 which is channel 6 on ADC1
 #define SIGNAL_SIZE 12000 // Assume that a single word is at most 1.5 sec
 #define NUMBER_OF_SIGNALS 11
 
 
 
-short int *all_signals[NUMBER_OF_SIGNALS];
+typedef short signed int Sample;
+typedef Sample* Signal;
+
+// Global Array to store all signals
+Signal all_signals[NUMBER_OF_SIGNALS];
+
 
 
 /********************* MAIN FUNCTION IS THE LAST FUNCTION *********************/
@@ -33,11 +39,19 @@ void delay(int ms){
 	vTaskDelay(ms / portTICK_RATE_MS);
 }
 
-void listen(void){
-
+// This function reads the analog pin and stores the samples in a signal
+void listen(Signal destination){
+	for(short unsigned int n = 0; n < SIGNAL_SIZE; n++){
+		if(gpio_get_level(LISTEN_PIN)){
+			destination[n] = adc1_get_raw(ADC1_CHANNEL_6);
+		}
+		else{
+			return;
+		}
+	}
 }
 
-void train()
+
 
 void app_main(void)
 {
