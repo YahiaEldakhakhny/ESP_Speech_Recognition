c71fdf208c82371df6eafd17cc1cb3db99caed98
YahiaEldakhakhny <yahia4360673@gmail.com>
2022-12-09 17:10:25 +0200

    auxilairy functions done

--- Arduino_programming/ESP_Speech/ESP_Speech.ino
+++ Arduino_programming/ESP_Speech/ESP_Speech.ino
@@ -13,6 +13,8 @@
 // TODO: change pins to be suitable for hardware connections
 #define PIN_MODE_TRAIN 10
 #define PIN_MODE_RECOGNIZE 11
+#define PIN_LISTEN 12 // pin used to tell esp to start listening
+#define PIN_MIC 34 // pin connected to mic, used to read analog values
 
 typedef double Sample;
 typedef double* RI_Vector;// Real or Imaginary vector
@@ -28,6 +30,13 @@ arduinoFFT FFT = arduinoFFT();
  
 void setup()
 {
+  // Setup for digital pins
+  pinMode(PIN_MODE_TRAIN, INPUT);
+  pinMode(PIN_MODE_RECOGNIZE, INPUT);
+  pinMode(PIN_LISTEN, INPUT);
+  // Setup for analog pin
+  pinMode(PIN_MIC, INPUT);
+    
   // Allocate memory space for all signals
   for(int i=0; i < NUMBER_OF_SIGNALS; i++){
     signal_setup(all_signals[i]);
@@ -41,9 +50,17 @@ void setup()
  
 void loop()
 {
+  // TODO: Write the main logic of loop
+  // if the user wants to be in the Recognition mode
+  while(digitalRead(PIN_MODE_RECOGNIZE)){
+    if(digitalRead(PIN_LISTEN)){
+      
+    }
+  }
       
 }
 
+// Function used for testing it serves no actual purpose
 double get_max_value(double vector[]){
   double max = vector[0];
   for(int i=1; i< SIGNAL_LENGTH; i++){
@@ -65,4 +82,28 @@ void signal_setup(Signal s){
   s = (Signal) malloc(2 * sizeof(RI_Vector));
   s[REAL] =(RI_Vector) malloc(SIGNAL_LENGTH * sizeof(Sample));
   s[IMAG] =(RI_Vector) malloc(SIGNAL_LENGTH * sizeof(Sample));
+
+  // Initialize the real and imaginary arrays with all zeros
+  for(int i = 0; i < SIGNAL_LENGTH; i++){
+    s[REAL][i] = 0;
+    s[IMAG][i] = 0;
+  }
 }
+
+// Function that reads analog values and stores them in a signal
+void listen(Signal s){
+  int i = 0;
+  while(digitalRead(PIN_LISTEN) && i < SIGNAL_LENGTH){
+    // analogRead returns a uint16_t so we need to cast it to sample
+    Sample currSample = (Sample) analogRead(PIN_MIC);
+    s[REAL][i] = currSample;
+    i++;
+    delay(1);
+  }
+}
+
+
+
+
+
+
